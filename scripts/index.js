61fafb3379f323cfb126f0bfc5e13723701e85e5
Daniil Kuznetsov <dankuzi@yandex.ru>
2023-02-12 23:29:47 +0400

    feat: реализовать открытие второго попапа

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,21 @@ const editButton = document.querySelector(".profile__edit-button");
 
 const popup = document.querySelector(".popup");
 
+const popupPlace = document.querySelector(".popup__place");
+
 const closeButton = popup.querySelector(".popup__close");
+
+const addButton = document.querySelector(".profile__add-button");
+
+const closeButtonSecond = document.querySelector(".popup__close-second");
+
+const toggleOpenPopupPlace = () => {
+  popupPlace.classList.toggle("popup_opened");
+}
+
+const toggleClosePopupPlace = () => {
+  popupPlace.classList.toggle("popup_opened");
+}
 
 function toggleOpenPopup(){
   popup.classList.toggle("popup_opened");
@@ -37,3 +51,5 @@ formElement.addEventListener('submit', handleFormSubmit);
 
 editButton.addEventListener("click", toggleOpenPopup);
 closeButton.addEventListener("click", toggleClosePopup);
+addButton.addEventListener("click", toggleOpenPopupPlace);
+closeButtonSecond.addEventListener("click", toggleClosePopupPlace);
